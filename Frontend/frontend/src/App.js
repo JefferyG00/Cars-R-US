1a51e7b84b5d07fc1a10e012baf8e973d12a0b5c
Jeffery Grigsby <jeffery.grigsby@yahoo.com>
2024-06-26 17:43:09 -0700

    fixed problem with flask

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, } from 'react';
+import axios from 'axios';
 import './components/styles.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
